4ce7d0275f41101f4c126d2c2080ad03822077a8
longdamhihi <n10648046@qut.edu.au>
2023-10-24 23:18:48 +0700

    update start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import StartGameScreen from "./components/StartGameScreen";
+import { InGameScreen } from "./components/InGameScreen";
 
 function App() {
-  return <div className="App"><StartGameScreen/></div>;
+  const [isStartScreen, setIsStartScreen] = useState<boolean>(true);
+  const [isInGameScreen, setIsInGameScreen] = useState<boolean>(false);
+
+  const handleClickStart = () => {
+    setIsStartScreen(!isStartScreen);
+    setIsInGameScreen(true);
+  };
+  return (
+    <div className="App">
+      {isStartScreen && <StartGameScreen onClickStart={handleClickStart} />}
+      {isInGameScreen && <InGameScreen />}
+    </div>
+  );
 }
 
 export default App;
--- src/components/StartGameScreen.tsx
+++ src/components/StartGameScreen.tsx
@@ -1,23 +1,49 @@
 import React from "react";
 import styled from "styled-components";
 
-const StartGameScreen = () => {
+interface Props {
+  onClickStart: () => void;
+}
+
+const StartGameScreen = ({ onClickStart }: Props) => {
   return (
-    <StyledButton>
-      Start
-    </StyledButton>
+    <StyledContainer>
+      <StyledTitle>Welcome to React Quiz Game!</StyledTitle>
+      <StyledButton onClick={onClickStart}>Start</StyledButton>
+    </StyledContainer>
   );
 };
 
 export default StartGameScreen;
+
+const StyledTitle = styled.div`
+  color: white;
+  text-align: center;
+  font-size: 3rem;
+  line-height: 1;
+  font-weight: 700;
+`;
+
+const StyledContainer = styled.div`
+  width: 1000px;
+  height: 200px;
+  margin: 64px auto;
+`;
 
 const StyledButton = styled.button`
   width: 170px;
   height: 52px;
   border-radius: 10px;
   border: none;
-  background-color: #6ED5B7;
+  background-color: #6ed5b7;
   font-weight: 700;
   font-size: 1.125rem;
   line-height: 1.75rem;
-`
+  margin: 24px auto;
+  display: block;
+  box-sizing: border-box;
+  &:hover {
+    color: white;
+    background-color: #639647fa;
+  }
+`;
