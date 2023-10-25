78bd73f61aac0057b1fdb6a3158a80e1f7778590
Ramboo103 <ramboo3110@gmail.com>
2023-10-25 16:37:34 +0700

    Long commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 import StartGameScreen from "./components/StartGameScreen";
-import { InGameScreen } from "./components/InGameScreen";
+import InGameScreen from "./components/InGameScreen";
+import EndGameScreen from "./components/EndGameStart";
 
 function App() {
   const [isStartScreen, setIsStartScreen] = useState<boolean>(true);
   const [isInGameScreen, setIsInGameScreen] = useState<boolean>(false);
-
+  const [isSumittedScreen, setIsSumittedScreen] = useState<boolean>(false);
+  const [isReviewedMode, setIsReviewedMode] = useState<boolean>(false);
+  const [finalScore, setFinalScore] = useState<number>(0);
+  
   const handleClickStart = () => {
     setIsStartScreen(!isStartScreen);
     setIsInGameScreen(true);
   };
+
+  const handleSubmitGame = (score: number) => {
+    setFinalScore(score);
+    setIsSumittedScreen(true);
+    setIsInGameScreen(false);
+  }
+  const handlerReviewClicked = () => {
+    setIsReviewedMode(true);
+    setIsInGameScreen(true);
+    setIsSumittedScreen(false);
+  }
+  
   return (
     <div className="App">
       {isStartScreen && <StartGameScreen onClickStart={handleClickStart} />}
-      {isInGameScreen && <InGameScreen />}
+      {isInGameScreen && <InGameScreen onSubmit={handleSubmitGame} isReviewedMode={isReviewedMode} />}
+      {isSumittedScreen && <EndGameScreen score={finalScore} onReviewClicked={handlerReviewClicked}/>}
     </div>
   );
 }
